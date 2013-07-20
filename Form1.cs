75429b0e6993dc2bd642e148307d68f6f2a64e61
Lasse Vang Gravesen <gravesenlasse@gmail.com>
2013-07-20 23:05:15 +0200

    Gathered all code related to MPC Logging and started a project where it runs in the background, and sits in the tray waiting for mpc-hc.exe to launch and when it does it looks at the file argument and logs that.
    This instead of overriding mpc-hc as the primary launcher for media files, and launching it through a different program after having intercepted the file argument and logged it.

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MPCLogger5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.CreateTrayIconMenu();
+            this.Hide();
+            notifyIcon1.Visible = true;
+
+            ProcessLogger.Start();
+
+        }
+
+        private void CreateTrayIconMenu()
+        {
+            ContextMenu cm = new ContextMenu();
+
+            MenuItem CloseProgram = new MenuItem();
+            CloseProgram.Text = String.Format("Close {0}", this.Text);
+            CloseProgram.Click += new EventHandler(CloseProgram_click);
+            cm.MenuItems.Add(CloseProgram);
+
+            notifyIcon1.ContextMenu = cm;
+        }
+
+        private void CloseProgram_click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // Do nothing.
+        }
+
+    }
+}
